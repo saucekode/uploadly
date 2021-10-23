d132e05a30eb08c424bbadc3d593d71299578ba2
saucekode <saucekoder@gmail.com>
2021-10-23 16:43:27 +0100

    pushing initial files

---
+++ src/main/java/com/app/uploadly/UploadlyApplication.java
@@ -0,0 +1,13 @@
+package com.app.uploadly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/uploadly/UploadlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.uploadly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
